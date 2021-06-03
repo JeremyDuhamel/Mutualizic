023ca33abfc47005abf89916e1cb7c67d31e614b
Tinerion <tinerion@pop-os.localdomain>
2021-06-03 09:30:25 +0200

    Add nodeIntegration support

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 const update = require('update-electron-app')
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const url = require('url')
 
 function createWindow () {
     const win = new BrowserWindow({
         width: 1366,
         height: 768,
         backgroundColor: '#181920',
-        icon: __dirname + '/ui/img/favicon.png',
+        icon: __dirname + 'favico.ico',
         webPreferences: {
-            preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
-            enableRemoteModule: true
+            contextIsolation: false,
+            preload: path.join(__dirname, 'preload.js'),
+            enableRemoteModule: true,
         },
         autoHideMenuBar: true,
     })
